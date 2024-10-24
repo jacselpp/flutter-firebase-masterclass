31c80e164720fbee274f192f579bfc3e8353f682
Jacsel <jacselpp@gmail.com>
2024-10-24 16:43:27 -0400

    Android Done

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -3,9 +3,15 @@ import 'package:ecommerce_app/src/app_bootstrap_fakes.dart';
 import 'package:ecommerce_app/src/app_bootstrap.dart';
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
   // create an app bootstrap instance
